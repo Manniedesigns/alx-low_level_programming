d88dbacb8cb7fd78072331ddfa1290b3cdfffa96
Manniedesigns <emmanuelchimyz2@gmail.com>
2022-09-08 16:30:17 -0700

    Hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,13 +6,15 @@
  *
  *   *
  *
- *    * Description: prints with proper grammar, but the outcome is a piece of art,
+ *    * Description: prints "with proper grammar, but the outcome is a piece of
  *
- *     *
+ *     * art,"
  *
- *      * Return: Always 0 (Success)
+ *      *
  *
- *       */
+ *       * Return: Always 0 (Success)
+ *
+ *        */
 
 int main(void)
 {
