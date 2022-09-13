9a60e81c6904851e098cd8712108605a2d0cc4c5
Manniedesigns <emmanuelchimyz2@gmail.com>
2022-09-13 16:37:54 -0700

    C - Functions and Nested Loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,26 +17,29 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		prod = j * i;
+		for (j = 0; j <= 9; j++)
+		{
+			prod = j * i;
 
-		if (j == 0)
-		{
-			_putchar('0');
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else if (prod <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(prod + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(prod / 10 + '0');
+				_putchar(prod % 10 + '0');
+			}
 		}
-		else if (prod <= 9)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(prod + '0');
-		}
-		else
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(prod / 10 + '0');
-			_putchar(prod % 10 + '0');
-		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
